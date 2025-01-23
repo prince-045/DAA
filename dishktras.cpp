20bd01f11ea2e1f83f0d51e123418b0dabd6f115
prince-045 <162796292+prince-045@users.noreply.github.com>
2025-01-23 16:25:43 +0530

    Update dishktras.cpp

--- dishktras.cpp
+++ dishktras.cpp
@@ -1,136 +1,83 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <climits>
 #include <chrono>
+#include <queue>
+
 using namespace std;
-#define INF 10000000
-int minKey(const vector<int>& key, const vector<bool>& mstSet, int V) {
-    int min = INF, minIndex = -1;
-    for (int v = 0; v < V; ++v) {
-        if (!mstSet[v] && key[v] < min) {
-            min = key[v];
-            minIndex = v;
-        }
-    }
-    return minIndex;
-}
+using namespace std::chrono;
+struct Edge {
+    int dest;
+    int weight;
+};
 
 
-void primMST(const vector<vector<int>>& graph, int V) {
-    vector<int> parent(V, -1); 
-    vector<int> key(V, INF); 
-    vector<bool> mstSet(V, false); 
-    key[0] = 0; 
+void dijkstra(int V, vector<vector<Edge>>& graph, int start) {
+    vector<int> dist(V, INT_MAX);  
+    dist[start] = 0;
 
-    auto start = chrono::high_resolution_clock::now();
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, start}); 
 
-    for (int count = 0; count < V - 1; ++count) {
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
 
-        int u = minKey(key, mstSet, V);
-        
-        mstSet[u] = true; 
+        for (auto& edge : graph[u]) {
+            int v = edge.dest;
+            int weight = edge.weight;
 
-        for (int v = 0; v < V; ++v) {
-            if (graph[u][v] != INF && !mstSet[v] && graph[u][v] < key[v]) {
-                key[v] = graph[u][v];
-                parent[v] = u;
-            }
-        }
-    }
-
-    auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-
-    cout << "Prim's Algorithm executed in: " << duration.count() << " microseconds" << endl;
-
-
-    cout << "MST edges from Prim's Algorithm:" << endl;
-    for (int i = 1; i < V; ++i) {
-        cout << parent[i] << " - " << i << " (Weight: " << graph[i][parent[i]] << ")" << endl;
-    }
-}
-
-
-int findParent(int v, vector<int>& parent) {
-    if (parent[v] == -1)
-        return v;
-    return parent[v] = findParent(parent[v], parent);
-}
-
-void unionSets(int x, int y, vector<int>& parent, vector<int>& rank) {
-    int xroot = findParent(x, parent);
-    int yroot = findParent(y, parent);
     
-    if (rank[xroot] < rank[yroot]) {
-        parent[xroot] = yroot;
-    } else if (rank[xroot] > rank[yroot]) {
-        parent[yroot] = xroot;
-    } else {
-        parent[yroot] = xroot;
-        rank[xroot]++;
-    }
-}
-
-
-void kruskalMST(const vector<vector<int>>& graph, int V) {
-    vector<pair<int, pair<int, int>>> edges;
-    vector<int> parent(V, -1);
-    vector<int> rank(V, 0);
-
-
-    for (int i = 0; i < V; ++i) {
-        for (int j = i + 1; j < V; ++j) {
-            if (graph[i][j] != INF) {
-                edges.push_back({graph[i][j], {i, j}});
+            if (dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
             }
         }
     }
-
-  
-    sort(edges.begin(), edges.end());
-
-    auto start = chrono::high_resolution_clock::now();
-
-    cout << "MST edges from Kruskal's Algorithm:" << endl;
-    for (auto edge : edges) {
-        int weight = edge.first;
-        int u = edge.second.first;
-        int v = edge.second.second;
-
-        int uroot = findParent(u, parent);
-        int vroot = findParent(v, parent);
-
-  
-        if (uroot != vroot) {
-            cout << u << " - " << v << " (Weight: " << weight << ")" << endl;
-            unionSets(uroot, vroot, parent, rank);
-        }
+    cout << "Vertex\tDistance from Source\n";
+    for (int i = 0; i < V; i++) {
+        cout << i << "\t" << (dist[i] == INT_MAX ? "INF" : to_string(dist[i])) << endl;
     }
-
-    auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-
-    cout << "Kruskal's Algorithm executed in: " << duration.count() << " microseconds" << endl;
 }
 
 int main() {
- 
-    int V = 5;
-    vector<vector<int>> graph(V, vector<int>(V, INF));
+    int V = 9;  
+    vector<vector<Edge>> graph(V);
 
-    graph[0][1] =12;
-    graph[0][2] =10;
-    graph[1][2] =8 ;
-    graph[1][3]=14 ;
-    graph[2][3] =13;
-    graph[2][4] =11;
-    graph[3][4] =15;
+    graph[0].push_back({1, 4});
+    graph[0].push_back({7, 8});
+    graph[1].push_back({0, 4});
+    graph[1].push_back({2, 8});
+    graph[1].push_back({7, 11});
+    graph[2].push_back({1, 8});
+    graph[2].push_back({3, 7});
+    graph[2].push_back({5, 4});
+    graph[3].push_back({2, 7});
+    graph[3].push_back({4, 9});
+    graph[3].push_back({5, 14});
+    graph[4].push_back({3, 9});
+    graph[4].push_back({5, 10});
+    graph[5].push_back({2, 4});
+    graph[5].push_back({3, 14});
+    graph[5].push_back({4, 10});
+    graph[5].push_back({6, 2});
+    graph[6].push_back({5, 2});
+    graph[6].push_back({7, 1});
+    graph[7].push_back({0, 8});
+    graph[7].push_back({1, 11});
+    graph[7].push_back({6, 1});
+    graph[7].push_back({8, 7});
+    graph[8].push_back({7, 7});
+    graph[8].push_back({6, 6});
 
-    cout << "Prim's Algorithm:" << endl;
-    primMST(graph, V);
-    cout << endl;
-    cout << "Kruskal's Algorithm:" << endl;
-    kruskalMST(graph, V);
+    int start = 0; 
+
+     auto start_time = high_resolution_clock::now();
+    dijkstra(V, graph, start);
+
+    auto end_time = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end_time - start_time);
+    cout << "Time taken: " << duration.count() << " microseconds" << endl;
 
     return 0;
 }
